3b5aba2e55a9d6a9e26b1d77be1067632861a048
justsayno <contact@sethreid.co.nz>
2016-11-27 10:53:11 +1300

    init

---
+++ src/DotnetReact.Web/ClientApp/components/Home.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default class Home extends React.Component<any, void> {
+    public render() {
+        return <div>
+            <h1>Hello, world!</h1>
+        </div>;
+    }
+}
